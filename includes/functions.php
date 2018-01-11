f8b26ef35f9bb7b465fed766fa69d061f49c7845
Rylee <ravereecken@gmail.com>
2018-01-11 14:06:17 -0500

    Added Version and small Plugin Card Shortcodes

--- includes/functions.php
+++ includes/functions.php
@@ -50,7 +50,8 @@ function do_ratings() {
     $body = wp_remote_retrieve_body( $response );
 
     $data = json_decode( $body ); ?>
-        <label>Plugin Rating: </label>
+
+        <label class="ratings">Plugin Rating: </label>
         <span style="background: url(<?php echo asset( 'images/stars.png' ) ?>) 0 -16px repeat-x" class="stars">
         
             <span style="background: url(<?php echo asset( 'images/stars.png' ) ?>) 0 0px repeat-x; width: <?php echo $data->rating ?>%;"></span>
@@ -81,3 +82,58 @@ function do_downloads() {
 }
 
 add_shortcode( 'plugin-info-downloads', 'pinfo\do_downloads' );
+
+function do_faqs() {
+  
+    ob_start();
+    
+    $url = "https://api.wordpress.org/plugins/info/1.0/" . get_option('plugin_name', 'ucare-support-system') . ".json";
+    $response = wp_remote_get( $url ); 
+        
+    $body = wp_remote_retrieve_body( $response );
+
+    $data = json_decode( $body );
+    
+    echo $data->sections->faq;
+
+    return ob_get_clean();
+    
+}
+
+add_shortcode( 'plugin-info-faqs', 'pinfo\do_faqs' );
+
+function do_plugin_card() {
+    
+    ob_start();
+    
+    $url = "https://api.wordpress.org/plugins/info/1.0/" . get_option('plugin_name', 'ucare-support-system') . ".json";
+    $response = wp_remote_get( $url ); 
+        
+    $body = wp_remote_retrieve_body( $response );
+
+    $data = json_decode( $body ); ?>
+    
+        <div class="pinfo-card">
+            
+            <div class="title"><?php echo $data->name ?></div>
+            <div class="ratings">
+                
+                <label class="ratings">Plugin Rating: </label>
+                <span style="background: url(<?php echo asset( 'images/stars.png' ) ?>) 0 -16px repeat-x" class="stars">
+        
+                    <span style="background: url(<?php echo asset( 'images/stars.png' ) ?>) 0 0px repeat-x; width: <?php echo $data->rating ?>%;"></span>
+            
+                </span>
+                
+            </div>
+            <div class="download">
+                <a href="<?php echo $data->download_link ?>">Download</a>
+            </div>
+            
+        </div>
+        
+    <?php return ob_get_clean();
+    
+}
+
+add_shortcode( 'plugin-info-card', 'pinfo\do_plugin_card');
